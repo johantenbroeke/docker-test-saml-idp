e23160dbddb89e39bb8709dd1d8cd8ea93d0244e
Johan ten Broeke <fa-broekej@srv000676.org.jstd.local>
2020-10-15 16:02:00 +0200

    added some test users

--- config/simplesamlphp/authsources.php
+++ config/simplesamlphp/authsources.php
@@ -18,6 +18,17 @@ $config = array(
             'eduPersonAffiliation' => array('group2'),
             'email' => 'user2@example.com',
         ),
+        'user3:user3pass' => array(
+            'uid' => array('johantenbroeke'),
+            'emailaddress' => 'Johan@tenbroeke.com',
+            'http://schemas.xmlsoap.org/ws/2005/05/identity/claims/emailaddress' => 'Johan@tenbroeke.com'            
+        ),
+        'johan:johan' => array(
+            'uid' => array('johantenbroeke'),
+            'emailaddress' => 'Johan@tenbroeke.com',
+            'attributes' => "dWlkPXVybiUzQWNvbGxhYiUzQXBlcnNvbiUzQWp1c3RpZC5ubCUzQWJyb2VrZWomYXV0aHNwX2xldmVsPTEwJnNlbF9hdXRoc3A9aHR0cHMlM0ElMkYlMkZlbmdpbmUuaWRwYnJva2VyLmFjY3Zlbmoucmlqa3N3ZWIubmwlMkZhdXRoZW50aWNhdGlvbiUyRmlkcCUyRm1ldGFkYXRhJmF1dGhzcF90eXBlPXNhbWwyMCZhdXRob3JpdHk9aHR0cHMlM0ElMkYlMkZhbG9naW4uanVzdGlkLm1pbnZlbmoubmwmZW1haWxhZGRyZXNzPUouVGVuQnJvZWtlJTQwanVzdGlkLm5sJmNsaWVudF9pcD0xMC40OC4xOTUuOTcmbGFuZ3VhZ2U9bmwmaHR0cCUzQSUyRiUyRnNjaGVtYXMueG1sc29hcC5vcmclMkZ3cyUyRjIwMDUlMkYwNSUyRmlkZW50aXR5JTJGY2xhaW1zJTJGZW1haWxhZGRyZXNzPUouVGVuQnJvZWtlJTQwanVzdGlkLm5sJnNlbF9sZXZlbD0xMCZuYW1lX2lkPXVybiUzQWNvbGxhYiUzQXBlcnNvbiUzQWp1c3RpZC5ubCUzQWJyb2VrZWomdXNlcl9hZ2VudD1Nb3ppbGxhJTJGNS4wKyUyOFdpbmRvd3MrTlQrMTAuMCUzQitXaW42NCUzQit4NjQlMjkrQXBwbGVXZWJLaXQlMkY1MzcuMzYrJTI4S0hUTUwlMkMrbGlrZStHZWNrbyUyOStDaHJvbWUlMkY4Ni4wLjQyNDAuNzUrU2FmYXJpJTJGNTM3LjM2",
+            'http://schemas.xmlsoap.org/ws/2005/05/identity/claims/emailaddress' => 'Johan@tenbroeke.com'            
+        ),
     ),
 
 );
